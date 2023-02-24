5be042d94cfcb0ca53d3998106a4820cc0de0d7c
Caitlin <113987933+437478@users.noreply.github.com>
2023-02-24 21:31:03 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function notification(){
+    var fullName = document.getElementById('fullName').innerHTML;
+    alert("Thank You" + fullName + " For Applying To Hot Beans Website Development Company");
+}
